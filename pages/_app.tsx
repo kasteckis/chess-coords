dfdafe60c75aa6bccd7fb7f7888463baead6e631
kasteckis <valentinas@kasteckis.lt>
2023-01-22 19:12:15 +0200

    Added prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
